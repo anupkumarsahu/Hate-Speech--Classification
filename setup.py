b61e12924c43fd0c83e54266ead58cac4434d2ce
Anup Kumar Sahu <anup-kumar.sahu@hpe.com>
2023-09-04 06:22:32 +0530

    Added Model

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-from setuptools 
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Anup",
+    author_email="anup.sahu@gmail.com",
+    package_dir=find_packages(),
+    install_requires=[],
+    
+)
